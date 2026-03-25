ae6099f4479c3cda6b5d9dbaebf16eba75b03e36
Viktor <v.kostov@sap.com>
2026-03-25 09:16:03 +0200

    Use local Backupbucket and Backupentry

--- pkg/gardenadm/cmd/init/options.go
+++ pkg/gardenadm/cmd/init/options.go
@@ -35,9 +35,6 @@ type Options struct {
 	Zone string
 
 	Bootstrap bool
-
-	// StoreContainer is the store container identifier for etcd backup/restore.
-	StoreContainer string
 	// NoMCM skips deployment of machine-controller-manager and any worker-related steps.
 	NoMCM bool
 }
@@ -100,6 +97,5 @@ func (o *Options) addFlags(fs *pflag.FlagSet) {
 	fs.BoolVar(&o.UseBootstrapEtcd, "use-bootstrap-etcd", false, "If set, the control plane continues using the bootstrap etcd instead of transitioning to etcd-druid. This is useful for testing purposes to save time.")
 	fs.StringVarP(&o.Zone, "zone", "z", "", "Availability zone for the new node. Required if the control plane worker pool in the `Shoot` has multiple zones configured. Optional if exactly one zone is configured (applied automatically). Must not be set if no zones are configured.")
 	fs.BoolVar(&o.Bootstrap, "bootstrap", false, "If set, only bootstap")
-	fs.StringVar(&o.StoreContainer, "store-container", "", "The store container identifier for etcd backup/restore.")
 	fs.BoolVar(&o.NoMCM, "no-mcm", false, "If set, skip deploying machine-controller-manager and worker-related components.")
 }
