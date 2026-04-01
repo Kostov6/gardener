77ab92066381886a3c31f2532a80418451bb5b56
Viktor <v.kostov@sap.com>
2026-04-01 10:21:54 +0300

    feature: Introduce gardenadm bootstrap --recover

--- pkg/gardenadm/cmd/bootstrap/bootstrap.go
+++ pkg/gardenadm/cmd/bootstrap/bootstrap.go
@@ -94,6 +94,11 @@ func run(ctx context.Context, opts *Options) error {
 		return fmt.Errorf("failed determining migrated extension kinds: %w", err)
 	}
 
+	command := fmt.Sprintf("%s%s init -d %q --log-level=%s", botanist.ImageVectorOverrideEnv(), nodeinit.GardenadmBinaryPath, botanist.ManifestsDir, opts.LogLevel)
+	if opts.Recover {
+		command = command + " --secret-file=/secrets.yaml --use-bootstrap-etcd"
+	}
+
 	var (
 		g = flow.NewGraph("bootstrap")
 
@@ -292,12 +297,7 @@ func run(ctx context.Context, opts *Options) error {
 		bootstrapControlPlane = g.Add(flow.Task{
 			Name: "Bootstrapping control plane on the first control plane machine",
 			Fn: flow.TaskFn(func(ctx context.Context) error {
-				return b.SSHConnection().RunWithStreams(ctx, nil, opts.Out, opts.ErrOut,
-					fmt.Sprintf("%s%s init -d %q --log-level=%s",
-						botanist.ImageVectorOverrideEnv(),
-						nodeinit.GardenadmBinaryPath, botanist.ManifestsDir, opts.LogLevel,
-					),
-				)
+				return b.SSHConnection().RunWithStreams(ctx, nil, opts.Out, opts.ErrOut, command)
 			}).Timeout(30 * time.Minute),
 			Dependencies: flow.NewTaskIDs(deployDNSRecord, copyManifests),
 		})
