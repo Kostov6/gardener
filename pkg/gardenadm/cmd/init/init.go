249b81a89f628091fd28f628380eff87964e8e2e
Viktor <v.kostov@sap.com>
2026-04-01 11:24:58 +0300

    hack: increase retry timeout

--- pkg/gardenadm/cmd/init/init.go
+++ pkg/gardenadm/cmd/init/init.go
@@ -165,7 +165,7 @@ func run(ctx context.Context, opts *Options) error {
 			Fn: flow.Parallel(
 				b.Components.RuntimeResourceManager.Wait,
 				b.Shoot.Components.ControlPlane.ResourceManager.Wait,
-			),
+			).RetryUntilTimeout(1*time.Minute, 5*time.Minute),
 			Dependencies: flow.NewTaskIDs(deployGardenerResourceManager),
 		})
 		_ = g.Add(flow.Task{
@@ -189,7 +189,7 @@ func run(ctx context.Context, opts *Options) error {
 		})
 		waitUntilExtensionControllersReady = g.Add(flow.Task{
 			Name:         "Waiting until extension controllers report readiness",
-			Fn:           b.WaitUntilExtensionControllerInstallationsHealthy,
+			Fn:           flow.TaskFn(func(ctx context.Context) error { return b.WaitUntilExtensionControllerInstallationsHealthy(ctx) }).RetryUntilTimeout(1*time.Minute, 5*time.Minute),
 			Dependencies: flow.NewTaskIDs(deployExtensionControllers),
 		})
 		deployNetworkPolicies = g.Add(flow.Task{
@@ -289,7 +289,7 @@ func run(ctx context.Context, opts *Options) error {
 			Fn: flow.Parallel(
 				b.Components.RuntimeResourceManager.Wait,
 				b.Shoot.Components.ControlPlane.ResourceManager.Wait,
-			),
+			).RetryUntilTimeout(time.Minute, 15*time.Minute),
 			SkipIf:       podNetworkAvailable,
 			Dependencies: flow.NewTaskIDs(deployGardenerResourceManagerIntoPodNetwork),
 		})
@@ -303,7 +303,7 @@ func run(ctx context.Context, opts *Options) error {
 		})
 		waitUntilExtensionControllersInPodNetworkReady = g.Add(flow.Task{
 			Name:         "Waiting until extension controllers (in pod network) report readiness",
-			Fn:           b.WaitUntilExtensionControllerInstallationsHealthy,
+			Fn:           flow.TaskFn(b.WaitUntilExtensionControllerInstallationsHealthy).RetryUntilTimeout(1*time.Minute, 10*time.Minute),
 			SkipIf:       podNetworkAvailable,
 			Dependencies: flow.NewTaskIDs(deployExtensionControllersIntoPodNetwork),
 		})
@@ -411,7 +411,7 @@ func run(ctx context.Context, opts *Options) error {
 					b.Shoot.Components.ControlPlane.ResourceManager.Wait,
 				),
 				b.WaitUntilExtensionControllerInstallationsHealthy,
-			).RetryUntilTimeout(time.Second, 5*time.Minute),
+			).RetryUntilTimeout(time.Second, 10*time.Minute),
 			Dependencies: flow.NewTaskIDs(waitUntilKubeControllerManagerIsActive),
 		})
 		deployMachineControllerManager = g.Add(flow.Task{
