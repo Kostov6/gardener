9616616879167ee6dc5706382ec5db1c843efce7
Viktor <v.kostov@sap.com>
2026-08-17 16:51:05 +0300

    Pin bootstrap extensions to control plane node

--- pkg/gardenlet/controller/controllerinstallation/controllerinstallation/reconciler.go
+++ pkg/gardenlet/controller/controllerinstallation/controllerinstallation/reconciler.go
@@ -713,12 +713,24 @@ func (r *Reconciler) MutateSpecForSelfHostedShootExtensions(obj runtime.Object) 
 			kubernetesutils.InjectKubernetesServiceHostEnv(podSpec.InitContainers, "localhost")
 			kubernetesutils.InjectKubernetesServiceHostEnv(podSpec.Containers, "localhost")
 
+			// Pin to the control plane node: in bootstrap mode the pods talk to the API server via 'localhost', which only works there.
+			podSpec.Affinity = &corev1.Affinity{NodeAffinity: &corev1.NodeAffinity{
+				RequiredDuringSchedulingIgnoredDuringExecution: &corev1.NodeSelector{
+					NodeSelectorTerms: []corev1.NodeSelectorTerm{{
+						MatchExpressions: []corev1.NodeSelectorRequirement{{
+							Key:      v1beta1constants.LabelNodeRoleControlPlane,
+							Operator: corev1.NodeSelectorOpExists,
+						}},
+					}},
+				},
+			}}
+
 			// extensions must even start on unready nodes in order to deploy the CNI plugin (nodes only become ready
 			// when networking is available)
 			podSpec.Tolerations = append(podSpec.Tolerations, corev1.Toleration{Operator: corev1.TolerationOpExists, Effect: corev1.TaintEffectNoSchedule})
 		}
 
-		podSpec.Tolerations = append(podSpec.Tolerations, corev1.Toleration{Key: "node-role.kubernetes.io/control-plane", Operator: corev1.TolerationOpExists})
+		podSpec.Tolerations = append(podSpec.Tolerations, corev1.Toleration{Key: v1beta1constants.LabelNodeRoleControlPlane, Operator: corev1.TolerationOpExists})
 	})
 }
 
